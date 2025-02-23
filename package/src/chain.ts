0d8e6b406eebc097256c389ea2786f9e9fd43776
Vordgi <sasha2822222@gmail.com>
2025-02-23 20:00:01 +0400

    nmc-26 improve package types

--- package/src/chain.ts
+++ package/src/chain.ts
@@ -1,5 +1,5 @@
-import { type NextRequest, type NextFetchEvent } from "next/server";
-import { type ChainItem, type ChainConfig } from "./lib/types";
+import { type NextFetchEvent } from "next/server";
+import { type ChainItem, type ChainConfig, type BaseRequest } from "./lib/types";
 import { collectData } from "./lib/collect-data";
 import { formatResponse } from "./lib/format-response";
 import { Logger } from "./lib/logger";
@@ -7,9 +7,13 @@ import { Logger } from "./lib/logger";
 export { FinalNextResponse } from "./lib/final-next-response";
 
 export const chain =
-    (middlewares: ChainItem[], config?: ChainConfig) => async (req: NextRequest, event: NextFetchEvent) => {
+    <RequestType extends Request & BaseRequest, ResponseType extends Response>(
+        middlewares: ChainItem<RequestType, ResponseType>[],
+        config?: ChainConfig,
+    ) =>
+    async (req: RequestType, event: NextFetchEvent) => {
         const logger = new Logger(config?.logger);
-        const summary = await collectData(req, event, middlewares, logger);
+        const summary = await collectData<RequestType, ResponseType>(req, event, middlewares, logger);
         const next = formatResponse(summary);
 
         return next;
